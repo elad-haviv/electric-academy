408db4f1c5e1a9925f508f8e3aa3781031f31010
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-09-29 18:12:45 +0300

    implemented authentication with clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    RedirectToSignIn,
+    SignIn,
+    SignUp,
+} from "@clerk/clerk-react";
 
 import MasterLayout from "./components/UI/MasterLayout";
 import AuthLayout from "./components/UI/AuthLayout";
@@ -10,6 +18,12 @@ import Home from "./routes/Home";
 import Learn from "./routes/Learn";
 import Dashboard from "./routes/Dashboard";
 import Simulator from "./components/Simulator/Simulator";
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+    throw new Error("Missing authentication service key");
+}
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 const router = createBrowserRouter([
     {
@@ -88,6 +102,31 @@ const router = createBrowserRouter([
     },
 ]);
 
+const authRouter = createBrowserRouter([
+    {
+        path: "/auth",
+        children: [
+            {
+                path: "/auth/sign-in/*",
+                element: <SignIn />,
+            },
+            {
+                path: "/auth/sign-up/*",
+                element: <SignUp />,
+            },
+        ],
+    },
+]);
+
 export default function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <ClerkProvider publishableKey={clerkPubKey}>
+            <SignedIn>
+                <RouterProvider router={router} />
+            </SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+        </ClerkProvider>
+    );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        cors: true,
-    },
-    plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+    return {
+        server: {
+            host: "0.0.0.0",
+            cors: true,
+        },
+        plugins: [react()],
+    };
 });
