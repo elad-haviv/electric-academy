01819629ec29f2515298655b565968f3e93f6db5
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-10-01 18:49:19 +0300

    refactored to ES6 module style

--- server/app.js
+++ server/app.js
@@ -1,41 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from "http-errors";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import mongoose from "mongoose";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from "./routes/index";
+import questionsRouter from "./routes/questions";
+import simulatorRouter from "./routes/simulator";
+import learnRouter from "./routes/learn";
+import { config } from "dotenv";
 
-var app = express();
+config();
+const app = express();
+
+// Database connection
+mongoose
+    .connect(`${process.env.DB_ADDRESS}/${process.env.DB_COLLECTIONs}`)
+    .then(() => console.log("Established connection to database"))
+    .catch((err) => console.error("Error connecting to database:", err));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// routers setup
+app.use("/", indexRouter);
+app.use("/questions", questionsRouter);
+app.use("/simulator", simulatorRouter);
+app.use("/learn", learnRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
-module.exports = app;
+export default app;
--- server/routes/questions.js
+++ server/routes/questions.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/all", function (req, res, next) {
     res.render("index", { title: "Express" });
 });
 
